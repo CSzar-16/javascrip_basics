8049abb26f5695ef4f4129b9cb84b80c36fbd0a2
Chirag Sandhekar <59355618+Czarsen@users.noreply.github.com>
2025-05-16 11:41:37 +0000

    conversions done

---
+++ 04_basics.js
@@ -0,0 +1,8 @@
+let score = "32abc"
+let scored = true
+console.log(typeof score)
+console.log(typeof(scored))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
