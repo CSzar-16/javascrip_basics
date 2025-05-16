d45a118d18d85d7a8aada3e6618941589a50611c
Chirag Sandhekar <59355618+chintuC2@users.noreply.github.com>
2025-05-16 04:21:44 +0000

    02 done

---
+++ 02_basics/02_constVar.js
@@ -0,0 +1,8 @@
+const accountId = 16005016
+let accountEmail = "crypto@gmail.com"
+var accountPswd = "Crypto"
+accountCity = "Vile Parle"
+accountPswd = "winter"
+// accountId = 2
+console.log(accountId);
+console.log(accountPswd)
