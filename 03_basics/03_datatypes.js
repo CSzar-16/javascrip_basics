ea935f59ccbc1cb71e2cbb656fc825288eb31186
Chirag Sandhekar <59355618+Czarsen@users.noreply.github.com>
2025-05-16 11:10:38 +0000

    data types done

---
+++ 03_basics/03_datatypes.js
@@ -0,0 +1,15 @@
+//let name = "Chirag"
+let age = 28
+let isLoggedIn = false
+// number  => 2 to power of 53
+//bigint
+//string => " ---- "
+// boolean => true or false
+//null = standalone value
+// undefined = value not assigned
+//symbol => unique
+//object
+console.log(typeof " Chirag")
+console.log(typeof 55)
+console.log(typeof true)
+console.log(typeof null)
