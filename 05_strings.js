9f814fe4ff4926e16181eba8f48b3f37996a82af
Chirag Sandhekar <59355618+CSzar-16@users.noreply.github.com>
2025-05-18 02:33:39 +0000

    string done

---
+++ 05_strings.js
@@ -0,0 +1,8 @@
+const name = "chirag "
+let repoCount = 27
+
+// older way probably not recommended
+console.log(name + repoCount);
+
+// string interpolation modern way 
+console.log(`Hello my name is ${name} and my age is ${repoCount}`)
