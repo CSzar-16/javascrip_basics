23285ae8845a1c58c7aa67666d70a62675d8433e
Chirag Sandhekar <59355618+CSzar-16@users.noreply.github.com>
2025-05-18 05:44:04 +0000

    math and num repo done

---
+++ 06_nums.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score)
+
+//creating static type of datatype predefined number type of variable
+const balance = new Number(200)
+console.log(balance);
+
+//using decimals precision through toPrecision method
+const numberOne = 34.94
+console.log(numberOne.toPrecision());
+
+
+// create numeric values commas according to indian system
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+/* ++++++++++++++++++++++++++   Maths Library       +++++++++++++++++++++++++++++++++++++++++++++*/
+ 
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*10 + 1))
+
+console.log(Math.floor(Math.random()*10 + 1))
